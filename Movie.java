3af7f0383411bdba5ed534ab1fb8f04829d9b22f
Daniel dos Anjos <danieldosanjos.36@gmail.com>
2022-07-26 09:00:38 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -44,4 +44,15 @@ public class Movie {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        int frequentRenterPoints = 0;
+        // add frequent renter points
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE) &&
+                daysRented > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }

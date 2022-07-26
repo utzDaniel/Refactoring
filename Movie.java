2f20ae727de245a931d4aff1715e6982f6c88ace
Daniel dos Anjos <danieldosanjos.36@gmail.com>
2022-07-26 09:13:03 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -1,53 +1,46 @@
 public class Movie {
 
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
+   public static final int CHILDRENS = 2;
+   public static final int REGULAR = 0;
+   public static final int NEW_RELEASE = 1;
 
-    private String _title;
-    private Price _price;
+   private String _title;
+   private Price _price;
 
-    public Movie(String name, int priceCode) {
-        _title = name;
-        setPriceCode(priceCode);
-     }
+   public Movie(String name, int priceCode) {
+      _title = name;
+      setPriceCode(priceCode);
+   }
 
-     public int getPriceCode() {
-        return _price.getPriceCode();
-     }
+   public int getPriceCode() {
+      return _price.getPriceCode();
+   }
 
-     public void setPriceCode(int arg) {
-        switch (arg) {
-           case REGULAR:
-              _price = new RegularPrice();
-              break;
-           case CHILDRENS:
-              _price = new ChildrensPrice();
-              break;
-           case NEW_RELEASE:
-              _price = new NewReleasePrice();
-              break;
-           default:
-              throw new IllegalArgumentException("Incorrect Price Code");
-        }
-     }
+   public void setPriceCode(int arg) {
+      switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
+   }
 
-    public String getTitle() {
-        return _title;
-    }
+   public String getTitle() {
+      return _title;
+   }
 
-    public double getCharge(int daysRented) {
-        return _price.getCharge(daysRented);
-    }
+   public double getCharge(int daysRented) {
+      return _price.getCharge(daysRented);
+   }
 
-    public int getFrequentRenterPoints(int daysRented) {
-        int frequentRenterPoints = 0;
-        // add frequent renter points
-        frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if ((getPriceCode() == Movie.NEW_RELEASE) &&
-                daysRented > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
-    }
+   public int getFrequentRenterPoints(int daysRented) {
+      return _price.getFrequentRenterPoints(daysRented);
+   }
 }

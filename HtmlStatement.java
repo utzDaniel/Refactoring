30dc001f609ad8c4b0661b9f6990e740bb56b6b9
Daniel dos Anjos <danieldosanjos.36@gmail.com>
2022-07-26 10:03:14 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,21 +1,4 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-
-        public String value(Customer aCustomer) {
-                Enumeration rentals = aCustomer.getRentals();
-
-                String result = header(aCustomer);
-
-                while (rentals.hasMoreElements()) {
-                        Rental each = (Rental) rentals.nextElement();
-                        
-                        result += body(each);
-                }
-
-                result += footer(aCustomer);
-                return result;
-        }
 
         public String header(Customer aCustomer) {
                 return "<H1>Rentals for <EM>" + aCustomer.getName() +
--- Statement.java
+++ Statement.java
@@ -2,7 +2,20 @@ import java.util.Enumeration;
 
  public abstract class Statement {
     
-    public abstract String value(Customer aCustomer);
+    public String value(Customer aCustomer){
+        Enumeration rentals = aCustomer.getRentals();
+
+        String result = header(aCustomer);
+
+        while (rentals.hasMoreElements()) {
+                Rental each = (Rental) rentals.nextElement();
+                
+                result += body(each);
+        }
+
+        result += footer(aCustomer);
+        return result;
+    }
 
     public abstract String header(Customer aCustomer);
 
